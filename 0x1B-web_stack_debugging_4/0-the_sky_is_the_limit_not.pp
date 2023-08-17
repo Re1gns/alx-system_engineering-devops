7e12f690d0e3e778cf87d36ebb50236a61217094
Reigns <117748085+Re1gns@users.noreply.github.com>
2023-08-17 16:46:29 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
 # Modifies the nginx default file descriptor
 exec { 'modify nginx':
- # provider => 'shell',
+  provider => 'shell',
+  path => '/usr/local/bin:/bin'
   command     => 'sed -i "s/15/1024/g" /etc/default/nginx; service nginx restart',
 }
