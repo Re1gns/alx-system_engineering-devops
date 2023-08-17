e74099055ca28c971a87b4056792b04bbd4f1532
Reigns <117748085+Re1gns@users.noreply.github.com>
2023-08-17 16:43:06 +0100

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,10 +2,10 @@
 
 exec{'holberton hard':
 	path => ["/bin"],
-	command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 4096/" /etc/security/limits.conf',
+	command => 'sed -i "s/^holberton hard nofile 4/holberton hard nofile 4096/" /etc/security/limits.conf',
 }
 
 exec{'soft holberton':
 	path => ["/bin"],
-	command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 4096/" /etc/security/limits.conf',
+	command => 'sed -i "s/^holberton soft nofile 4/holberton soft nofile 4096/" /etc/security/limits.conf',
 }
