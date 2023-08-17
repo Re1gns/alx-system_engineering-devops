f670e110fa1a5de7125103eea829524a591e7b96
Reigns <117748085+Re1gns@users.noreply.github.com>
2023-08-17 17:07:35 +0100

    fix 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,12 @@
-# Configures holberton user
+# Fix problem of high amount files opened
 
-exec{'holberton hard':
-	path => ["/bin"],
-	command => 'sed -i "s/^holberton hard nofile 4/holberton hard nofile 4096/" /etc/security/limits.conf',
+exec {'replace-1':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['replace-2'],
 }
 
-exec{'soft holberton':
-	path => ["/bin"],
-	command => 'sed -i "s/^holberton soft nofile 4/holberton soft nofile 4096/" /etc/security/limits.conf',
+exec {'replace-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
