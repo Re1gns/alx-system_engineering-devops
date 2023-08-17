162825a00e03bb3e8bb652073c3f73f3bef3d7a7
Reigns <117748085+Re1gns@users.noreply.github.com>
2023-08-17 17:05:51 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,12 @@
-# Modifies the nginx default file descriptor
-exec { 'modify nginx':
-  provider => 'shell',
-  path => '/usr/local/bin:/bin'
-  command     => 'sed -i "s/15/4096/" /etc/default/nginx; service nginx restart',
+# Fix problem of high amount of requests
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
